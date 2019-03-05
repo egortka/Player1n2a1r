4632ef031c98ac3989a066f0b36796f9346c6e1c
Egor Tkachenko <egortka@egors-imac.local>
2019-03-05 21:26:12 +0700

    Initial Commit

---
+++ Player1n2a1r/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Player1n2a1r
+//
+//  Created by Egor Tkachenko on 05/03/2019.
+//  Copyright © 2019 ET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
